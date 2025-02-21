b822cd293a6936d6725bac0f8c6b98136cb7e60f
mdikavw <mdikavw@gmail.com>
2025-02-21 22:01:25 +0700

    Created sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,23 @@
-<div class="w-64 bg-[#1975c5] h-full">
-    <ul style="list-style-type: none; padding: 0;">
-        <li>Beranda</li>
+<div class="w-64 px-6 py-12 bg-[#1975c5] h-full text-white">
+    <div class="flex justify-center">
+        <img class="w-16 h-auto" src="{{ asset('images/logo_kementrian.png') }}" alt="Kementrian Logo">
+    </div>
+    <ul class="flex flex-col h-full gap-4 p-8 font-medium text-md">
+        <li class="flex items-center gap-4">
+            <span class="fa-solid fa-home"></span>
+            <span>Beranda</span>
+        </li>
+        <li class="flex items-center gap-4">
+            <span class="fa-regular fa-circle-check"></span>
+            <span>Checklist</span>
+        </li>
+        <li class="flex items-center gap-4">
+            <span class="fa-solid fa-clipboard"></span>
+            <span>Rekapitulasi</span>
+        </li>
+        <li class="flex items-center gap-4 mt-auto mb-8">
+            <span class="fa-solid fa-right-from-bracket"></span>
+            <span>Keluar</span>
+        </li>
     </ul>
 </div>
