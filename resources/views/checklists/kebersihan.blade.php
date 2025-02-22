390f157897b7f9dbabab844c816ec190a96a8c5d
Erlangga <elangrama22@gmail.com>
2025-02-22 16:20:55 +0700

    add curbside and kebersihan

---
+++ resources/views/checklists/kebersihan.blade.php
@@ -0,0 +1,16 @@
+@extends('checklist')
+
+@section('checklist-title', 'Kebersihan Gedung Terminal')
+
+@section('checklist-content')
+        <h1 class="text-2xl font-bold mb-2">Checklist Kebersihan Terminal</h1>
+        <p class="font-bold text-gray-700">Kamis, 13 Februari 2025</p>
+        
+        <div class="mt-4">
+            <button class="bg-gray-300 text-black px-4 py-2 rounded flex items-center">
+                Periode 2 07:00 - 09:00
+                <span class="ml-2">▼</span>
+            </button>
+        </div>
+
+@endsection
