cc5c6fb1b6134781f887bf4e51cb32f330526fec
mdikavw <mdikavw@gmail.com>
2025-03-02 00:16:45 +0700

    Updated cors insecure content policy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>@yield('title', 'My Laravel App')</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
