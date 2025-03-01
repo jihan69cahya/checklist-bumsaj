f3812c99ce8ef263abbfc96e73cc7a62c2e7d62d
mdikavw <mdikavw@gmail.com>
2025-03-02 00:42:59 +0700

    Edited css and js loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>@yield('title', 'My Laravel App')</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('js/app.js') }}" rel="script">
+
     </head>
 
     <body class="bg-gray-100">
