04960671f747d2a2d92a3c7b282e0de115311b3b
mdikavw <mdikavw@gmail.com>
2025-03-02 00:05:32 +0700

    Undo installing vite plugin vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
     plugins: [
@@ -8,7 +7,6 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        vercel(),
     ],
     server: {
         https: true,
