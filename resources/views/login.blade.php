cc4c0af8c8805b58223940ebf6f8fed7ae419904
mdikavw <mdikavw@gmail.com>
2025-03-03 04:19:34 +0700

    Trying vite resource calling

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,13 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>Login</title>
-        @if (app()->environment('local'))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @if (app()->environment('local'))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
             @php $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true); @endphp
             <link href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}" rel="stylesheet">
             <script src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}" defer></script>
-        @endif
+        @endif --}}
     </head>
 
     <body class="flex items-center justify-center min-h-screen"
