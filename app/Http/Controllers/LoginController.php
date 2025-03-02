765cda60da640d8bad863f4ad147738c956ef867
mdikavw <mdikavw@gmail.com>
2025-03-03 04:14:11 +0700

    Fix periode berakhir design

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
         {
             $request->session()->regenerate();
 
-            return redirect()->intended('dashboard');
+            return redirect()->intended('/');
         }
 
         return back()->withErrors([
